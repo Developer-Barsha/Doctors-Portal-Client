cd79dd826b77263ee154b5f2824e9a0436734e89
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-05-12 14:30:35 +0600

    dynamic modal set

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -26,16 +26,8 @@ const AvailableAppointments = ({ date }) => {
                             setTreatment={setTreatment}
                         />)
                 }
-                {treatment && <BookingModal treatment={treatment} />}
+            {treatment && <BookingModal date={selectedDate} treatment={treatment} setTreatment={setTreatment} />}
             </div>
-            {/* <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-            <div class="modal">
-                <div class="modal-box relative">
-                    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                </div>
-            </div> */}
         </section >
     );
 };
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,17 +1,43 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment }) => {
+const BookingModal = ({ treatment, date, setTreatment }) => {
+    const theDate = format(date, 'PP');
+    const {_id, name, slots} = treatment;
+
+    const handleBooking=e=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const phone = e.target.phone.value;
+        const slot = e.target.slot.value;
+
+        setTreatment(null)
+        console.log(_id, name, email, phone, slot);
+    }
+
     return (
         <div>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
-            <div className="modal modal-bottom sm:modal-middle">
+            <div className="modal modal-middle">
                 <div className="modal-box">
-                    <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="font-bold text-lg">Congratulations random Interner user!{treatment.name}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div className="modal-action">
+                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="font-bold text-lg mb-4">Booking For : <span className='text-secondary font-bold'>{name}</span></h3>
+
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-4'>
+                        <input type="text" className='w-full outline-none rounded p-2 py-1 bg-slate-200' value={theDate} />
+                        <select name='slot' className="select select-bordered w-full">
+                            {slots.map(slot=><option value={slot}>{slot}</option>)}
+                        </select>
+                        <input type="text" name='name' className='w-full rounded p-2 py-1 outline-none border-2' placeholder={'Full Name'} />
+                        <input type="number" name='phone' className='w-full rounded p-2 py-1 outline-none border-2' placeholder={'Phone Number'} />
+                        <input type="email" name='email' className='w-full rounded p-2 py-1 outline-none border-2' placeholder={'Email'} />
+                        <input type="submit" className='w-full text-white btn p-0' value={'Submit'} />
+                    </form>
+
+                    {/* <div className="modal-action">
                         <label for="booking-modal" className="btn">Yay!</label>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
