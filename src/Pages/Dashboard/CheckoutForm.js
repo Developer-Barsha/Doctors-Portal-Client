ec25216f814b286a0a825319d194706f9cad5be5
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-05-20 15:49:20 +0600

    problem solved

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,46 +1,107 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ appointment }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
 
-    const handleSubmit =async (e) => {
+    const { price, patient, patientname } = appointment;
+
+    useEffect(() => {
+        const url = 'http://localhost:5000/create-payment-intent';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret);
+                }
+            });
+
+    }, [price])
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return;
         }
 
-        const card = document.getElement(CardElement);
+        const card = elements.getElement(CardElement);
 
-        if(card===null){
+        if (card === null) {
             return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        setCardError(error?.message || '');
+        setSuccess('');
+
+        // cofirm card payment
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientname,
+                        email: patient
+                    },
+                },
+            },
+        );
+
+        if (intentError) {
+            setCardError(intentError?.message);
+        }
+        else {
+            setCardError('');
+            setSuccess('Congrats!!~ Your payment is completed!');
+            console.log(paymentIntent);
         }
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-sm btn-success mt-4' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+
+                <button className='btn btn-sm btn-success mt-4' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+
+            </form>
+            {cardError && <p className='text-error pt-4'>{cardError}</p>}
+            {success && <p className='text-success pt-4'>{success}</p>}
+        </>
     );
 };
 
--- src/Pages/Dashboard/__payment_steps.js
+++ src/Pages/Dashboard/__payment_steps.js
@@ -1,5 +1,12 @@
 /* 
   *  1. Install react stripe js
   *  2. Open stripe account on stripe website
-  *  3 . copy publishable key pk__
+  *  3. get publishable key pk__
+  *  4. Create Elements wrapper using publishable key
+  *  5. Create checkout form using CardElement, useStripe, useElements
+  *  6. get card elements info(credit card info)
+  *  7. get credit card info/error + display card error if error found
+  * -------------------
+  *  8. get client secret from backend via payment intent post api
+  *  9. store client secret on the client side
 */
